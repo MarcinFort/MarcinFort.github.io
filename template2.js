8e8b0f518c839d1f03bafa2c2f34d1b50ab6bb0b
Marcin Fortuna <marcin.fort@gmail.com>
2018-04-02 16:38:24 +0200

    Style the menu; add underline at scrolling

--- template2.js
+++ template2.js
@@ -1,7 +1,23 @@
 $(document).scroll(function() {
-    if( $(this).scrollTop() >= $(window).height() ) {
+    if( $(this).scrollTop() >= $(window).height() - 80) {
         $('#menu_bar').addClass("colored_bg");
     } else {
         $('#menu_bar').removeClass("colored_bg");
     }
+    if( $(this).scrollTop() >= 0) {
+        let currentPos = Math.ceil($(this).scrollTop() / $(window).height());
+        if (currentPos !== 3) {
+            $("#menu_options").find("div").removeClass();
+            $("#menu_options").find("div[id='a_"+currentPos+"']").addClass("active");
+        }
+    }
 })
+
+// $("#menu_options a").on("click", (e) => {
+//     e.preventDefault();
+
+// });
+
+$(window).on("hashchange", function () {
+    window.scrollTo(window.scrollX, window.scrollY + 1);
+});
